3fe951db6c504c6daddc6521f4f24e8d86541194
Dylan Priestley <dylan_1003@hotmail.com>
2020-05-11 23:58:59 +1000

    Algorithm finished, progress on GUI

--- FirewallGUI/MainWindow.xaml.cs
+++ FirewallGUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
-namespace FirewallGUI
+namespace FirewallGui
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string inputFileNames { get; set; }
+
+        public string outputFileName { get; set; }
+
+        string loneFieldOption { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnAddFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if(fileDialog.ShowDialog() == true)
+            {
+                addedFileList.Text = File.ReadAllText(fileDialog.FileName);
+                inputFileNames += fileDialog.FileName + ',';
+            }
+        }
+
+        private void startMerge_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- FirewallLibrary/DataManager.cs
+++ FirewallLibrary/DataManager.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Diagnostics;
-using System.IO.Pipes;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace FirewallLibrary
 {
@@ -17,30 +13,55 @@ namespace FirewallLibrary
         // Holds the rules in their current state
         private List<Rule> processedRules { get; set; }
 
-        // Getter for the original rules
+        private string lessUniqueField { get; set; }
+
+        // Public getter for the original rules
         public List<Rule> OriginalRules
         {
             get { return unalteredRules; }
         }
-        // Getter for processed rule list
+        // Public getter for processed rule list
         public List<Rule> ProcessedRules
         {
             get { return processedRules; }
         }
 
-        // Constructor for 
+        //Constructor
+        public DataManager()
+        {
+
+        }
+
+        // Constructor
         public DataManager(List<string> inputData)
         {
             unalteredRules = LoadDataSet(inputData);
+            lessUniqueField = GetFieldWithLowerUniqueOccurrences();
+        }
+        // Will help with algorithm later
+        private string GetFieldWithLowerUniqueOccurrences()
+        {
+            int uniquePorts = unalteredRules.GroupBy(rule => rule.server).Select(group => group.Count()).Max();
+
+            int uniqueServers = unalteredRules.GroupBy(rule => rule.server).Select(group => group.Count()).Max();
+            // Default to server if it's even
+            if(uniquePorts < uniqueServers)
+            {
+                return "port";
+            }
+            else
+            {
+                return "server";
+            }
         }
 
-        // Flat dedupe of data
+        // A dumb dedupe of data
         private static void Dedupe(ref List<string> dedupedData)
         {
             dedupedData = dedupedData.Distinct().ToList();
         }
 
-        // Gets rid of rules that are functionally redundant (same server and port)
+        // Gets rid of rules that are functionally redundant (same server and port as another rule)
         public void FilterRedundantRules()
         {
             Console.WriteLine("Total rules: " + unalteredRules.Count); // O(1)
@@ -52,7 +73,6 @@ namespace FirewallLibrary
         static List<Rule> LoadDataSet(List<string> dataRows)
         {
             Dedupe(ref dataRows);
-
             List<Rule> dataSet = new List<Rule>();
 
             for (int i = 0; i < dataRows.Count; i++)
@@ -62,13 +82,45 @@ namespace FirewallLibrary
 
             return dataSet;
         }
+        // If an exclusion field option is selected
+        static List<Rule> LoadDataSet(List<string> dataRows,string[] exclusionFieldValues, string exclusionField)
+        {
+            Dedupe(ref dataRows);
+            List<Rule> dataSet = new List<Rule>();
+
+            for (int i = 0; i < dataRows.Count; i++)
+            {
+                dataSet.Add(new Rule(dataRows[i]));
+            }
+            foreach(string excludedData in exclusionFieldValues)
+            {
+                if(exclusionField == "rule_name")
+                {
+                    dataSet = dataSet.Where(rule => rule.rule_name != excludedData).ToList();
+                }
+                else if(exclusionField == "server")
+                {
+                    dataSet = dataSet.Where(rule => rule.server != excludedData).ToList();
+                }
+                else if(exclusionField == "port")
+                {
+                    dataSet = dataSet.Where(rule => rule.port != excludedData).ToList();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid exclusion field, skipping step...");
+                }
+            }
+
+            return dataSet;
+        }
 
         // For option to have rules that reference only one server or port
-        public void MergeRulesOnOnlyPortOrServer(string field = "")
+        public void MergeRulesOnOnlyPortOrServer(string fieldOption)
         {
             int newRuleNumber = 1;
 
-            if (field == "server")
+            if (fieldOption == "-ONEPORT")
             {
                 var servers = processedRules.Select(e => e.server).Distinct();
 
@@ -78,7 +130,7 @@ namespace FirewallLibrary
                     newRuleNumber++;
                 }
             }
-            else if(field == "port")
+            else if (fieldOption == "-ONESERVER")
             {
                 var ports = processedRules.Select(e => e.port).Distinct();
 
@@ -96,148 +148,90 @@ namespace FirewallLibrary
             HashSet<Rule> oldRuleList = processedRules.ToHashSet();
             HashSet<Rule> newRuleList = new HashSet<Rule>();
 
-            // Group on server and sort by number of times it appears
-            var servers = oldRuleList.GroupBy(rule => rule.server)
-                .Select(group => new { server = group.Key, count = group.Count() })
-                .OrderByDescending(group => group.count);
-
-            // Group on port and sort by number of times it appears
-            var ports = oldRuleList.GroupBy(rule => rule.port)
-                .Select(group => new { port = group.Key, count = group.Count() })
-                .OrderByDescending(group => group.count);
-
-#if DEBUG
-            Console.WriteLine(ports.Count());
-            Console.WriteLine(servers.Count());
-#endif      
-
-            //Set up variables for algorithm
-            int portsCountMax = ports.Select(p => p.count).Max();
-            int serversCountMax = servers.Select(s => s.count).Max();
-            // Get outer loop starting point
-            int outerStartPoint = (serversCountMax > portsCountMax ? serversCountMax : portsCountMax);
-
-            List<string> currentPorts = new List<string>();
-            List<string> currentServers = new List<string>();
-            
             int newRuleNumber = 0;
+            string currentKey;
+            string type;
 
-            // Starts with the elements that occur the most and works downwards
-            for (int i = outerStartPoint; i >= 0; i--) //O(n)
-            {
-                currentPorts = ports.Where(p => p.count == i).Select(e => e.port).ToList();//O(n3)
-                currentServers = servers.Where(s => s.count == i).Select(s => s.server).ToList();//O(n3)
-
-                foreach (string server in currentServers)
-                {
-                    if (oldRuleList.Any(rule => rule.server == server)) //O(n)
-                    {
-                        newRuleNumber++; //O(1)
-
-                        newRuleList.UnionWith(oldRuleList.Where(rule => rule.server == server)
-                            .Select(rule => new Rule { rule_name = "R-" + newRuleNumber, server = rule.server, port = rule.port })); //O(n3)
-
-                        oldRuleList.RemoveWhere(rule => rule.server == server);//O(n2)
-                    }
-                }
-                foreach (string port in currentPorts)
-                {
-                    if (oldRuleList.Any(rule => rule.port == port))
-                    {
-                        newRuleNumber++;
-
-                        newRuleList.UnionWith(oldRuleList.Where(rule => rule.port == port)
-                        .Select(rule => new Rule { rule_name = "R-" + newRuleNumber, server = rule.server, port = rule.port }));
-
-                        oldRuleList.RemoveWhere(rule => rule.port == port);
-                    }
-                }
-            }
             while (oldRuleList.Any())
             {
-                foreach (string server in currentServers)
+                currentKey = getMostOccurringField(oldRuleList, out type);
+                // We get which field we're working with so that we don't need to run collection.Any() in the loop, lower cost
+                if (type == "server")
                 {
-                    if (oldRuleList.Any(rule => rule.server == server)) //O(n)
-                    {
-                        newRuleNumber++; //O(1)
+                    newRuleNumber++;
 
-                        newRuleList.UnionWith(oldRuleList.Where(rule => rule.server == server)
-                            .Select(rule => new Rule { rule_name = "R-" + newRuleNumber, server = rule.server, port = rule.port })); //O(n3)
-
-                        oldRuleList.RemoveWhere(rule => rule.server == server);//O(n2)
-                    }
-                }
-                foreach (string port in currentPorts)
-                {
-                    if (oldRuleList.Any(rule => rule.port == port))
-                    {
-                        newRuleNumber++;
-
-                        newRuleList.UnionWith(oldRuleList.Where(rule => rule.port == port)
+                    newRuleList.UnionWith(oldRuleList.Where(rule => rule.server == currentKey)
                         .Select(rule => new Rule { rule_name = "R-" + newRuleNumber, server = rule.server, port = rule.port }));
 
-                        oldRuleList.RemoveWhere(rule => rule.port == port);
-                    }
+                    oldRuleList.RemoveWhere(rule => rule.server == currentKey);
+                }
+                else if(type == "port")
+                {
+                    newRuleNumber++;
+
+                    newRuleList.UnionWith(oldRuleList.Where(rule => rule.port == currentKey)
+                        .Select(rule => new Rule { rule_name = "R-" + newRuleNumber, server = rule.server, port = rule.port }));
+
+                    oldRuleList.RemoveWhere(rule => rule.port == currentKey);
                 }
             }
+            processedRules = newRuleList.ToList();
+        } 
 
-            if (!oldRuleList.Any())
+        //Below is high cost but using hashsets should alleviate it to a degree
+        //Need to find highest grouped count each iteration as taking out one field takes from the other, the numbers shift
+        private string getMostOccurringField(HashSet<Rule> ruleSet, out string type)
+        {
+            // Group by and get highest count for each field
+            KeyValuePair<string, int> server = ruleSet.GroupBy(rule => rule.server)
+                .OrderByDescending(group => group.Count())
+                .Select(group => new KeyValuePair<string, int>(group.Key, group.Count()))
+                .First();
+
+            KeyValuePair<string, int> port = ruleSet.GroupBy(rule => rule.port)
+                .OrderByDescending(group => group.Count())
+                .Select(group => new KeyValuePair<string, int>(group.Key, group.Count()))
+                .First();
+
+            // TieBreaker is whichever field has less unique occurrences, results in less rules total
+            if(server.Value > port.Value)
             {
-                processedRules = newRuleList.ToList();
+                type = "server";
+                return server.Key;
+            }
+            else if(port.Value > server.Value)
+            {
+                type = "port";
+                return port.Key;
             }
             else
             {
-                throw new Exception("Rules were missed, aborting.");
+                type = lessUniqueField;
+                return (lessUniqueField == "server" ? server.Key : port.Key);
             }
         }
 
-        /*
-        public int CalculateMaxPorts(HashSet<Rule> oldRuleList, HashSet<Rule> newRuleList, ref int portsCount, ref int serversCount)
-        {
-            var servers = oldRuleList.GroupBy(rule => rule.server)
-                .Select(group => new { server = group.Key, count = group.Count() })
-                .OrderByDescending(group => group.count).ToList();
-
-            var ports = oldRuleList.GroupBy(rule => rule.port)
-                .Select(group => new { port = group.Key, count = group.Count() })
-                .OrderByDescending(group => group.count).ToList();
-        }
-        
-        public void AnyPortsReferencedByAllRules()
-        {
-            var removerules = unalteredRules.Distinct().ToList();
-
-            int reqd = unalteredRules.Select(r => r.rule_name).Distinct().ToList().Count();
-
-            List<string> ports = new List<string>();
-
-            ports = removerules.GroupBy(p => p.port).Where(o => o.Count() == reqd).Select(e => e.Key).ToList();
-
-            foreach(string port in ports)
-            {
-                Console.WriteLine(port);
-                Console.ReadLine();
-            }
-        }
-        */
-
-        // Allows options for different result sets by resetting data, for possible GUI
+        // Allows options for different result sets by resetting and rerunning data operations, for possible GUI
         private void ResetData()
         {
             processedRules = unalteredRules;
         }
-
-        public void ProcessData(string option = "")
+        // Called by outer process
+        public void ProcessData(string loneFieldOption = "", string exclusionField = "", string exclusionValueList = "")
         {
+
+            loneFieldOption = loneFieldOption.ToUpper();
+
             FilterRedundantRules();
-            // AnyPortsReferencedByAllRules();
-            if (option.ToUpper() == "-ONEPORT")
+
+            if (loneFieldOption != "" && (loneFieldOption == "-ONEPORT" || loneFieldOption == "-ONESERVER"))
             {
-                MergeRulesOnOnlyPortOrServer("port");
+                MergeRulesOnOnlyPortOrServer(loneFieldOption);
             }
-            else if(option.ToUpper() == "-ONESERVER")
+            else if(loneFieldOption != "")
             {
-                MergeRulesOnOnlyPortOrServer("server");
+                Console.WriteLine("invalid -MergeField option, skipping.");
+                ConsolidateServersAndPorts();
             }
             else
             {
